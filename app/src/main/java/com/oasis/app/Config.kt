cf3d733fc98a71e55838bd7d7b7e0d22af2f30fa
restoreback5-tech <restoreback5@gmail.com>
2026-03-25 20:33:16 -0600

    Fix: desactivar STT temporalmente para debuggear

--- app/src/main/java/com/oasis/app/Config.kt
+++ app/src/main/java/com/oasis/app/Config.kt
@@ -5,6 +5,6 @@ object Config {
     const val ENABLE_TOASTS = true
     const val ENABLE_ANIMATIONS = true
     const val ENABLE_TTS = true
-    const val ENABLE_STT = true
+    const val ENABLE_STT = false
     var SAFE_MODE = false
 }
